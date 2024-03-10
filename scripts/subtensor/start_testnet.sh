ea5bb8fed360acc1b14941d751d14d7c40d6f5e5
HcL-CO <157390852+HcL-CO@users.noreply.github.com>
2024-03-10 18:40:32 -0400

    Update start_testnet.sh

--- scripts/subtensor/start_testnet.sh
+++ scripts/subtensor/start_testnet.sh
@@ -2,7 +2,7 @@ cd ~/subtensor/
 pm2 start ./target/release/node-subtensor \
     --name test_subtensor -- \
     --base-path /tmp/blockchain \
-    --chain ./raw_spec.json \
+    --chain ./raw_testspec.json \
     --rpc-external --rpc-cors all \
     --ws-external --no-mdns \
     --ws-max-connections 10000 --in-peers 500 --out-peers 500 \

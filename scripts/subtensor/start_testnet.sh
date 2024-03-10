514777b905e2ae20b60ce3019e614cd10763a71a
HcL-CO <157390852+HcL-CO@users.noreply.github.com>
2024-03-10 17:20:37 -0400

    Add files via upload

---
+++ scripts/subtensor/start_testnet.sh
@@ -0,0 +1,10 @@
+cd ~/subtensor/
+pm2 start ./target/release/node-subtensor \
+    --name test_subtensor -- \
+    --base-path /tmp/blockchain \
+    --chain ./raw_spec.json \
+    --rpc-external --rpc-cors all \
+    --ws-external --no-mdns \
+    --ws-max-connections 10000 --in-peers 500 --out-peers 500 \
+    --bootnodes /ip4/75.119.138.170/tcp/30333/ws/p2p/12D3KooWPrWvB2ZQ8Rn71uyPk9gZFTi7mqhkabka3ZkCXx7gaEBs \
+    --sync warp

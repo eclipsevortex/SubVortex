136820974e74b29a2c697149917b5981a20c2c8a
eclipsevortex <eclipsevortext@hotmail.com>
2024-05-08 10:33:18 +0100

    fix subtensor scripts

--- scripts/subtensor/docker/subtensor_docker_setup.sh
+++ scripts/subtensor/docker/subtensor_docker_setup.sh
@@ -15,8 +15,12 @@ fi
 # Go the repository
 cd subtensor
 
+# Checkout main branch
+git checkout main
+echo -e '\e[32mCheckout main branch\e[0m'
+
 # Get the latest version
-git pull
+git pull origin main
 echo -e '\e[32mLast version pulled\e[0m'
 
 # Go back to the current directory

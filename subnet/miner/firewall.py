ec35fe86f9bb18eba54fe11cdc4654bde1ee0b14
eclipsevortex <eclipsevortext@hotmail.com>
2024-06-19 14:36:49 +0100

    wip

--- subnet/miner/firewall.py
+++ subnet/miner/firewall.py
@@ -92,7 +92,7 @@ class Firewall(threading.Thread):
         """
         synapses = self.get_specification("synapses") or []
         if len(synapses) > 0 and name.lower() not in synapses:
-            return (True, RuleType.DENY, f"Synapse name '{name}' not found")
+            return (True, RuleType.DENY, f"Synapse name '{name}' not found, available {synapses}")
 
         return (False, None, None)
 

68a6b8017172ba08e7742e43d31d3e1ca2234dc7
eclipsevortex <eclipsevortext@hotmail.com>
2025-05-27 14:50:15 +0100

    implement challenger on valdiator

--- subvortex/validator/core/challenger/challenger.py
+++ subvortex/validator/core/challenger/challenger.py
@@ -159,7 +159,7 @@ class Challenger:
                     )
 
                     # Get the list of challengers for the scheduled country
-                    challengers: typing.List[cmm.Miner] = [
+                    challengees: typing.List[cmm.Miner] = [
                         m for m in miners if m.country == country
                     ]
 
@@ -168,10 +168,10 @@ class Challenger:
 
                     # Do not challenge if there are no challengers
                     challenge = None
-                    if len(challengers) > 0:
+                    if len(challengees) > 0:
                         # Wait next block to let miners proxy whitelisting the validator
                         btul.logging.debug(
-                            "Waiting miners proxy to be set up",
+                            "Waiting challengees proxy to be set up",
                             prefix=self.settings.logging_name,
                         )
                         await scbs.wait_for_block(subtensor=self.subtensor)
@@ -194,13 +194,27 @@ class Challenger:
                                 step_index=step_index,
                                 settings=self.settings,
                                 ip_occurences=ip_occurences,
-                                challengers=challengers,
+                                challengees=challengees,
                                 identities=identities,
                             )
                         )
                         challenge_time = time.time() - challenge_start_time
                         btul.logging.debug(
                             f"[{step_index}] Challenge completed in {challenge_time} seconds",
+                            prefix=self.settings.logging_name,
+                        )
+
+                        # Compute the scores
+                        score_start_time = time.time()
+                        await self._score_challengers(
+                            step_index=step_index,
+                            settings=self.settings,
+                            challengees=challengees,
+                            challenges_result=challenges_result,
+                        )
+                        score_time = time.time() - score_start_time
+                        btul.logging.debug(
+                            f"[{step_index}] Scoring completed in {score_time} seconds",
                             prefix=self.settings.logging_name,
                         )
                     else:
@@ -273,22 +287,22 @@ class Challenger:
         step_index: int,
         settings: sccs.Settings,
         ip_occurences: Counter,
-        challengers: typing.List[cmm.Miner],
+        challengees: typing.List[cmm.Miner],
         identities: typing.Dict[str, typing.List],
     ):
         # Define the result
         checks_result = {}
 
         # Define the available challengers
-        available_challengers = list(challengers)
+        available_challengers = list(challengees)
 
         # Execute some checks
-        for challenger in challengers:
+        for challenger in challengees:
             ip = challenger.ip
             hotkey = challenger.hotkey
 
         # Execute some checks
-        for challenger in challengers:
+        for challenger in challengees:
             ip = challenger.ip
             hotkey = challenger.hotkey
 
@@ -342,6 +356,151 @@ class Challenger:
         }
 
         return result, challenge
+
+    async def _score_challengers(
+        self,
+        step_index: int,
+        settings: sccs.Settings,
+        challengees: typing.List[cmm.Miner],
+        challenges_result: typing.Dict[str, ccm.ChallengeResult],
+    ):
+        # Compute the metadata
+        for challenger in challengees:
+            # Get the miner reason for the challenger
+            challenge_result: ccm.ChallengeResult = challenges_result.get(challenger.hotkey)
+
+            # Get the score of the challenger
+            score: cm.Score = scores.get(challenger.hotkey)
+            score.success = challenge_result.is_successful
+            score.reason = challenge_result.reason
+
+            # Display the hotkey
+            btul.logging.debug(
+                f"[{step_index}][{challenger.uid}] Hotkey: {challenger.hotkey}"
+            )
+
+            if challenge_result.chain:
+                btul.logging.debug(
+                    f"[{step_index}][{challenger.uid}] Challenge - Chain: {challenge_result.chain}, Type: {challenge_result.type}"
+                )
+
+            # Check if the miner/subtensor are verified
+            if not score.success:
+                btul.logging.warning(
+                    f"[{step_index}][{challenger.uid}] Challenge failed - {challenge_result.reason}",
+                    prefix=self.settings.logging_name,
+                )
+
+            # Availability metadata
+            score.availability_attempts, score.availability_successes = (
+                refresh_availability_metadata(
+                    settings=settings, result=challenge_result, score=score
+                )
+            )
+
+            # Reliability metadata
+            score.reliability_attempts, score.reliability_successes = (
+                refresh_reliability_metadata(
+                    settings=settings, result=challenge_result, score=score
+                )
+            )
+
+            # Latency metadata
+            score.latency_times = refresh_latency_metadata(
+                settings=settings, result=challenge_result, score=score
+            )
+
+            # Performance metadata
+            (
+                score.performance_attempts,
+                score.performance_successes,
+                score.performance_boost,
+            ) = refresh_performance_metadata(
+                settings=settings, result=challenge_result, score=score
+            )
+
+        # Compute Indivudal and Peers scores
+        # Individual score: score that is computed based on the miner itself
+        # Peers score: score that is computed based on the challenged miners
+        for challenger in challengees:
+            # Get the miner reason for the challenger
+            challenge_result: ccm.ChallengeResult = challenges_result.get(challenger.hotkey)
+
+            # Get the score of the challenger
+            score: cm.Score = scores.get(challenger.hotkey)
+
+            # Compute score for availability
+            score.availability_score = compute_availability_score(score=score)
+            btul.logging.debug(
+                f"[{step_index}][{challenger.uid}] Availability score {score.availability_score}",
+                prefix=self.settings.logging_name,
+            )
+
+            # Compute score for reliability
+            score.reliability_score = compute_reliability_score(score=score)
+            btul.logging.debug(
+                f"[{step_index}][{challenger.uid}] Reliability score {score.reliability_score}",
+                prefix=self.settings.logging_name,
+            )
+
+            # Compute score for latency
+            score.latency_score = compute_latency_score(
+                scores=scores,
+                challenger=challenger,
+            )
+            btul.logging.debug(
+                f"[{step_index}][{challenger.uid}] Latency score {score.latency_score}",
+                prefix=self.settings.logging_name,
+            )
+
+            # Compute score for performence
+            score.performance_score = compute_performance_score(
+                scores=scores,
+                challenger=challenger,
+            )
+            btul.logging.debug(
+                f"[{step_index}][{challenger.uid}] Performance score {score.performance_score}",
+                prefix=self.settings.logging_name,
+            )
+
+        # Computes Performance scores
+        # Performance score: score that is computed based on the x top miners
+        for challenger in challengees:
+            # Get the miner reason for the challenger
+            challenge_result: ccm.ChallengeResult = challenges_result.get(challenger.hotkey)
+
+            # Get the score of the challenger
+            score: cm.Score = scores.get(challenger.hotkey)
+
+            # Compute score for distribution
+            score.distribution_score = compute_distribution_score(
+                settings=settings,
+                challenger=challenger,
+                scores=scores,
+            )
+            btul.logging.debug(
+                f"[{step_index}][{challenger.uid}] Distribution score {score.distribution_score}",
+                prefix=self.settings.logging_name,
+            )
+
+            # Compute final score
+            score.final_score = compute_final_score(settings, score)
+            btul.logging.info(
+                f"[{step_index}][{challenger.uid}] Final score {score.final_score}",
+                prefix=self.settings.logging_name,
+            )
+
+            # Compute the moving score
+            # TODO: Take into account the fact that the miner maybe flag!
+            score.moving_score = (
+                settings.MOVING_SCORE_ALPHA * score.final_score
+                + (1 - settings.MOVING_SCORE_ALPHA) * score.moving_score
+            )
+            btul.logging.info(
+                f"[{step_index}][{challenger.uid}] Moving score {score.moving_score}",
+                prefix=self.settings.logging_name,
+            )
+
 
     def _is_validator(neuron: scmn.Neuron):
         return neuron.validator_trust > 0 and neuron.stake >= 1000
---
+++ subvortex/validator/core/challenger/score.py
@@ -0,0 +1,330 @@
+# The MIT License (MIT)
+# Copyright © 2025 Eclipse Vortex
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+import numpy as np
+from typing import Dict
+
+import bittensor.utils.btlogging as btul
+
+import subvortex.validator.core.challenger.settings as sccs
+import subvortex.validator.core.challenger.model as ccm
+import subvortex.validator.core.challenger.utils as ccu
+
+# Longest distance between any two places on Earth is 20,010 kilometers
+MAX_DISTANCE = 20010
+
+# Decay rate parameter controlling the distribution score decrease.
+BASE_SCORE = 1
+DECAY_RATE = 0.5
+
+
+def refresh_availability_metadata(
+    settings: sccs.Settings, result: ccm.ChallengeResult, score: Score
+):
+    attempts = ccu.apply_sma(settings, score.availability_attempts, 1, 0)
+    btul.logging.trace(
+        f"[{score.uid}][Score][Availability] # of attempts {score.availability_attempts}",
+        prefix=settings.logging_name,
+    )
+
+    successes = ccu.apply_sma(
+        settings,
+        score.availability_successes,
+        int(result.is_available),
+        0,
+    )
+    btul.logging.trace(
+        f"[{score.uid}][Score][Availability] # of successes {score.availability_successes}",
+        prefix=settings.logging_name,
+    )
+
+    return attempts, successes
+
+
+def compute_availability_score(score: Score):
+    """
+    Compute the availability score of the uid
+    """
+    # Compute the attempts/success
+    attempts = sum(score.availability_attempts)
+
+    successes = sum(score.availability_successes)
+
+    # Normalization
+    score = successes / attempts if attempts > 0 else 0
+
+    return score
+
+
+def refresh_reliability_metadata(
+    settings: sccs.Settings, result: ccm.ChallengeResult, score: Score
+):
+    attempts = ccu.apply_sma(settings, score.reliability_attempts, 1, 0)
+    btul.logging.trace(
+        f"[{score.uid}][Score][Reliability] # of attemmpts {score.reliability_attempts}",
+        prefix=settings.logging_name,
+    )
+
+    successes = ccu.apply_sma(
+        settings,
+        score.reliability_successes,
+        int(result.is_reliable),
+        0,
+    )
+    btul.logging.trace(
+        f"[{score.uid}][Score][Reliability] # of successes {score.reliability_successes}",
+        prefix=settings.logging_name,
+    )
+
+    return attempts, successes
+
+
+def compute_reliability_score(score: Score):
+    """
+    Compute the reliability score of the uid
+    """
+    # Compute the attempts/success
+    attempts = sum(score.reliability_attempts)
+
+    successes = sum(score.reliability_successes)
+
+    # Normalization
+    score = successes / attempts
+
+    return score
+
+
+def refresh_latency_metadata(
+    settings: sccs.Settings, result: ccm.ChallengeResult, score: Score
+):
+    latency_times = ccu.apply_sma(
+        settings,
+        score.latency_times,
+        result.avg_process_time,
+        settings.CHALLENGE_TIMEOUT,
+    )
+    btul.logging.trace(
+        f"[{score.uid}][Score][Latency] Process times {latency_times}",
+        prefix=settings.logging_name,
+    )
+    return latency_times
+
+
+# TODO: Put the score at 0 if the minimum attempts is not all successful! or find way to not get 1 when you are alone!
+def compute_latency_score(
+    settings: sccs.Settings,
+    scores: Dict[str, Score],
+    challenger: Neuron,
+):
+    """
+    Compute the latency score of the uid
+    """
+    # Get the challenger score
+    score = scores.get(challenger.hotkey)
+
+    # Compute the average latency for each challenger
+    latencies = [(x.hotkey, np.mean(x.latency_times).item()) for x in scores.values()]
+
+    # Sort the challenger SMA in descending way
+    latencies_sorted = sorted(latencies, key=lambda x: x[1])
+
+    # Find the rank of the challenger
+    challenger_rank = next(
+        (i for i, x in enumerate(latencies_sorted) if x[0] == challenger.hotkey), -1
+    )
+    if challenger_rank == -1:
+        return LATENCY_FAILURE_REWARD
+
+    btul.logging.info(f"Rank: {challenger_rank}", prefix=settings.logging_name)
+
+    # Calculate the adjustment factor
+    adjustment_factor = (TOP_X_MINERS - len(scores)) / TOP_X_MINERS
+
+    # Set the sign modifier based on the index
+    sign_modifier = -1 if challenger_rank == 0 else 1
+
+    # Override scores if there are less miners than top x miners
+    score = (
+        compute_score(challenger_rank)
+        if len(scores) >= TOP_X_MINERS
+        else compute_score(challenger_rank)
+        + (
+            sign_modifier
+            * adjustment_factor
+            * (compute_score(challenger_rank) - compute_score(challenger_rank + 1))
+        )
+    )
+
+    return score
+
+
+def refresh_performance_metadata(
+    settings: sccs.Settings, result: ccm.ChallengeResult, score: Score
+):
+    attempts = ccu.apply_sma(
+        settings, score.performance_attempts, result.challenge_attempts, 0
+    )
+    btul.logging.trace(
+        f"[{score.uid}][Score][Performance] # of attempts {score.performance_attempts}",
+        prefix=settings.logging_name,
+    )
+
+    successes = ccu.apply_sma(
+        settings,
+        score.performance_successes,
+        result.challenge_successes,
+        0,
+    )
+    btul.logging.trace(
+        f"[{score.uid}][Score][Performance] # of successes {score.performance_successes}",
+        prefix=settings.logging_name,
+    )
+
+    # Compute an boost for challengee request more attempts than teh minimum
+    total_attempts = sum(attempts)
+    total_successes = sum(successes)
+    success_ratio = (total_successes / total_attempts) if total_attempts > 0 else 0
+
+    # Apply a non-linear boost for challengers attempting more than PERFORMANCE_MIN_ATTEMPTS
+    attempt_boost = (
+        total_attempts / DEFAULT_CHALLENGE_MAX_ITERATION
+    ) ** settings.PERFORMANCE_REWARD_EXPONENT
+
+    # Penalize challengers who take more attempts but fail more
+    penalty = 1 - (1 - success_ratio) * settings.PERFORMANCE_PENALTY_FACTOR
+    boost = success_ratio * attempt_boost * penalty
+
+    boosts = ccu.apply_sma(settings, score.performance_boost, boost, 0)
+
+    return attempts, successes, boosts
+
+
+# TODO: Put the score at 0 if the minimum attempts is not all successful! or find way to not get 1 when you are alone!
+def compute_performance_score(
+    scores: Dict[str, Score],
+    challenger: Neuron,
+):
+    """
+    Compute the performance score of the uid
+    """
+    # Get the challenger score
+    score = scores.get(challenger.hotkey)
+
+    # Build the array of hotkey/boots
+    boosts = [(x.hotkey, np.mean(x.performance_boost).item()) for x in scores.values()]
+
+    # Sort challengers by performance score in descending order
+    performance_scores_sorted = sorted(boosts, key=lambda x: x[1], reverse=True)
+
+    # Find the rank of the challenger
+    challenger_rank = next(
+        (
+            i
+            for i, x in enumerate(performance_scores_sorted)
+            if x[0] == challenger.hotkey
+        ),
+        -1,
+    )
+    if challenger_rank == -1:
+        return PERFORMANCE_FAILURE_REWARD
+
+    # Calculate the adjustment factor
+    adjustment_factor = (TOP_X_MINERS - len(scores)) / TOP_X_MINERS
+
+    # Set the sign modifier based on the index
+    sign_modifier = -1 if challenger_rank == 0 else 1
+
+    # Override scores if there are less miners than top x miners
+    score = (
+        compute_score(challenger_rank)
+        if len(scores) >= TOP_X_MINERS
+        else compute_score(challenger_rank)
+        + (
+            sign_modifier
+            * adjustment_factor
+            * (compute_score(challenger_rank) - compute_score(challenger_rank + 1))
+        )
+    )
+
+    return score
+
+
+def compute_distribution_score(
+    settings: sccs.Settings,
+    challenger: Neuron,
+    scores: Dict[str, Score],
+):
+    """
+    Compute the distribution score of the challenger
+    Only the top x miner will receive a score, the rest will receive 0
+    """
+    # Sort the challengers per score - override distribution score to make it obsolete
+    sorted_challengers = sorted(
+        scores.values(),
+        key=lambda x: compute_final_score(settings, x, {"distribution": 1}),
+        reverse=True,
+    )
+
+    # Take the top X miners
+    top_challenger = sorted_challengers[:TOP_X_MINERS]
+
+    # Find the inder of the miner
+    challenger_rank = next(
+        (i for i, x in enumerate(top_challenger) if x.hotkey == challenger.hotkey), -1
+    )
+    if challenger_rank == -1:
+        return DISTRIBUTION_FAILURE_REWARD
+
+    # Compute the score using a exponential decay formula
+    # which is used in scoring systems to assign diminishing values to items based on their rank
+    score = compute_score(challenger_rank)
+
+    return score
+
+
+def compute_final_score(settings: sccs.Settings, score: Score, overrides={}):
+    """
+    Compute the final score based on the different scores (availability, reliability, latency and distribution)
+    """
+    availability_score = overrides.get("availability", score.availability_score)
+    latency_score = overrides.get("latency", score.latency_score)
+    reliability_score = overrides.get("reliability", score.reliability_score)
+    performance_score = overrides.get("performance", score.performance_score)
+    distribution_score = overrides.get("distribution", score.distribution_score)
+
+    numerator = (
+        (settings.AVAILABILITY_WEIGHT * availability_score)
+        + (settings.LATENCY_WEIGHT * latency_score)
+        + (settings.RELIABILITY_WEIGHT * reliability_score)
+        + (settings.DISTRIBUTION_WEIGHT * distribution_score)
+        + (settings.PERFORMANCE_WEIGHT * performance_score)
+    )
+
+    denominator = (
+        settings.AVAILABILITY_WEIGHT
+        + settings.LATENCY_WEIGHT
+        + settings.RELIABILITY_WEIGHT
+        + settings.DISTRIBUTION_WEIGHT
+        + settings.PERFORMANCE_WEIGHT
+    )
+
+    score = numerator / denominator if denominator != 0 else 0
+
+    return score
+
+
+def compute_score(index):
+    return BASE_SCORE * np.exp(-DECAY_RATE * index).item()

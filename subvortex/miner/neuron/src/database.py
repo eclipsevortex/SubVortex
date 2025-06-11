4c9700be329000c4fc83b43adbaec3a1d62a7b61
eclipsevortex <eclipsevortext@hotmail.com>
2025-06-11 11:51:07 +0200

    add missing piece for saving scores

--- subvortex/miner/neuron/src/database.py
+++ subvortex/miner/neuron/src/database.py
@@ -1,3 +1,8 @@
+import typing
+import traceback
+import bittensor.utils.btlogging as btul
+
+from subvortex.miner.neuron.src.models.score import MinerScore100, Score
 from subvortex.core.metagraph.database import NeuronReadOnlyDatabase
 
 
@@ -20,3 +25,86 @@ class Database(NeuronReadOnlyDatabase):
         super().__init__(settings=settings)
 
         self.setup_neuron_models()
+        self.models["score"] = {x.version: x for x in [MinerScore100()]}
+
+    async def get_scores(self) -> typing.List[Score]:
+        # Ensure the connection is up and running
+        await self.ensure_connection()
+
+        # Get the active versions
+        _, active = await self._get_migration_status("score")
+
+        for version in reversed(active):
+            model = self.models["score"][version]
+            if not model:
+                continue
+
+            try:
+                # Attempt to read all neurons using the model
+                neurons = await model.read_all(self.database)
+                return neurons
+
+            except Exception as ex:
+                btul.logging.warning(
+                    f"[get_scores] Failed to read all scores using version={version}: {ex}",
+                    prefix=self.settings.logging_name,
+                )
+                btul.logging.debug(
+                    f"[get_scores] Exception type: {type(ex).__name__}, Traceback:\n{traceback.format_exc()}",
+                    prefix=self.settings.logging_name,
+                )
+
+        return []
+
+    async def save_scores(self, score: Score, max_entries: int = 100):
+        """
+        Bulk update for a list of miners using active model versions.
+        """
+        await self.ensure_connection()
+
+        _, active = await self._get_migration_status("score")
+
+        for version in reversed(active):
+            model = self.models["score"][version]
+            if not model:
+                continue
+
+            try:
+                await model.write(self.database, score)
+
+            except Exception as ex:
+                btul.logging.warning(
+                    f"[{version}] Update score failed: {ex}",
+                    prefix=self.settings.logging_name,
+                )
+                btul.logging.debug(
+                    f"[update_score] Exception type: {type(ex).__name__}, Traceback:\n{traceback.format_exc()}",
+                    prefix=self.settings.logging_name,
+                )
+
+        return None
+
+    async def prune_scores(self, max_entries: int):
+        await self.ensure_connection()
+
+        _, active = await self._get_migration_status("score")
+
+        for version in reversed(active):
+            model = self.models["score"][version]
+            if not model:
+                continue
+
+            try:
+                await model.prune(self.database, max_entries)
+
+            except Exception as ex:
+                btul.logging.warning(
+                    f"[{version}] Update score failed: {ex}",
+                    prefix=self.settings.logging_name,
+                )
+                btul.logging.debug(
+                    f"[update_score] Exception type: {type(ex).__name__}, Traceback:\n{traceback.format_exc()}",
+                    prefix=self.settings.logging_name,
+                )
+
+        return None
